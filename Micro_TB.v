e5bdacfa9cbd025b3efbe9b4e41f854cddb253a1
dalbc <dalberthc@gmail.com>
2017-11-29 05:04:26 -0600

    Add files via upload

--- Micro_TB.v
+++ Micro_TB.v
@@ -1,102 +1,97 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 10/02/2017 06:59:43 PM
-// Design Name: 
 // Module Name: Micro_TB
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module Micro_TB(
     );
-    
-     reg reloj;
-     reg resetM;
-     wire [31:0] P_C;
-     wire [35:0] aux1;
-     wire [115:0] aux2;
-     wire [73:0] aux3;
-     wire [70:0] aux4;
-     wire [31:0] DI_banco,DO_D;
-     wire [4:0] DIR_WRA;
-     wire REG_WR;
-     reg [31:0] arreglo [0:31];
-     reg [31:0] mem [0:31];
-     
-     integer i = 0;
-     
- Micro_MIPS micro(
-        .reloj(reloj),
-        .resetM(resetM),
-        .P_C(P_C),
-        .aux1(aux1),
-        .aux2(aux2),
-        .aux3(aux3),
-        .aux4(aux4),
-        .DI_banco(DI_banco),
-        .DIR_WRA(DIR_WRA),
-        .REG_WR(REG_WR),
-        .DO_D(DO_D)
-);
 
-            
-            always
-            begin
-            #5 reloj = ~reloj;
-            end  
-  
-    
-initial 
-    
-    begin
-    
-    for (i = 0; i < 32; i = i + 1) ////// inicialiazar arreglo en 0
-        begin
-        arreglo[i] = {31'b0}; 
-        mem[i] = {31'b0};
-        end
-        
-   reloj = 0;
-   resetM = 1;
-   #30;
-   
-   resetM = 0;
-  
-   
-    
-    end
-  
+    reg reloj;
+    reg resetM;
+    wire [71:0] aux;
+    wire [31:0] P_C, addr2;
+    wire [31:0] DI_banco1, DI_banco2;
+    wire [4:0] DIR_WRA1, DIR_WRA2;
+    wire REG_WR1, REG_WR2;
+    wire [31:0] DO_D1, DO_D2;
+
+    reg [31:0] arreglo1 [0:31];
+    reg [31:0] mem1 [0:31];
+    reg [31:0] arreglo2 [0:31];
+    reg [31:0] mem2 [0:31];
+
+    integer i = 0;
+    integer j = 0;
+
+    Micro_MIPS micro(
+          .reloj(reloj),
+          .resetM(resetM),
+
+          .aux(aux),
+          .P_C(P_C), .addr2(addr2),
+          .DI_banco1(DI_banco1), .DI_banco2(DI_banco2),
+          .DIR_WRA1(DIR_WRA1), .DIR_WRA2(DIR_WRA2),
+          .REG_WR1(REG_WR1), .REG_WR2(REG_WR2),
+          .DO_D1(DO_D1), .DO_D2(DO_D2)
+    );
+
+
     always
     begin
-    @(posedge reloj);
-    if (P_C >= 12 && REG_WR == 0)
+    #5 reloj = ~reloj;
+    end
+
+
+    initial
+
     begin
-    arreglo[DIR_WRA] = {DI_banco}|{DO_D};
-    $display("%d: %h",DIR_WRA, arreglo[DIR_WRA]);
+
+    for (i = 0; i < 32; i = i + 1)    ////// inicialiazar arreglo en 0
+        begin
+        arreglo1[i] = {31'b0};
+        mem1[i] = {31'b0};
+        end
+
+    for (j = 0; j < 32; j = j + 1)    ////// inicialiazar arreglo en 0
+        begin
+        arreglo2[j] = {31'b0};
+        mem2[j] = {31'b0};
+        end
+
+    reloj = 0;
+    resetM = 1;
+
+    #30;
+    resetM = 0;
+
     end
-//    @(negedge aux3[72]);
-//    mem[aux3[42:37]]={aux3[36:5]};
-//    $display("%d: %h",aux3[42:37], mem[aux3[42:37]]);
+
+
+    always
+    begin
+      @(posedge reloj);
+      if (P_C >= 12 && REG_WR1 == 0)
+      begin
+        arreglo1[DIR_WRA1] = {DI_banco1}|{DO_D1};
+        $display("%d: %h", DIR_WRA1, arreglo1[DIR_WRA1]);
+      end
     end
-    
-    
-//    assign aux2 = {ALU_FUN, SEL_ALU, SEL_REG, ctrl_MEM_exe, ctrl_WB_exe, A, DOB_exe, imm_ext_exe, rt_exe, rd_exe};
-//    assign aux3 = {MEM_RD, MEM_WR, w_h, ctrl_WB_mem, DIR_D, DI_D, Y_MUX_mem};
-//    assign aux4 = {DIR_WB, REG_WR, DO_D_wb, DIR_D_wb, DIR_WRA};
-            
-          
-  endmodule        
-          
+/*
+    always
+    begin
+      @(posedge reloj);
+      if (P_C >= 12 && REG_WR2 == 0)
+      begin
+        arreglo2[DIR_WRA2] = {DI_banco2}|{DO_D2};
+        $display("%d: %h", "2", DIR_WRA2, arreglo2[DIR_WRA2]);
+      end
+      end
+*/
+
+
+endmodule
